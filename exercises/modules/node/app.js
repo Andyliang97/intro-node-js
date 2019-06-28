88168cb28d5fa66fad5d98d160629c4f63a1cb7b
AndyLiang97 <junjl97@vt.edu>
2019-06-28 18:02:46 -0400

    commit modules execises solution

---
+++ exercises/modules/node/app.js
@@ -0,0 +1,30 @@
+const {getUserById, getPostsForUser} = require('./api.js');
+const posts = require('./data');
+showPostsForCurrentUser = (userId, cb) => {
+  getPostsForUser(userId, posts => {
+    const postTemplates = posts.map(post => {
+      return `
+      <div class="post">
+        ${post.title}
+        ${post.body}
+        ${post.createdBy}
+      </div>`
+    })
+    cb(postTemplates)
+  })
+}
+
+showUserProfile = (userId, cb) => {
+  getUserById(userId, user => {
+    const profile = `
+      <div>
+        ${user.name}
+      </div>
+    `
+    cb(user)
+  })
+}
+
+module.exports={showPostsForCurrentUser, showUserProfile};
+
+
